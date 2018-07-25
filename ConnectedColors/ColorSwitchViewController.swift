59012ca711c08bc155daf07010e87ff9b3b39ddd
Ralf Ebert <info@ralfebert.de>
2018-07-25 17:18:05 +0200

    ConnectedColors example project

--- ConnectedColors/ColorSwitchViewController.swift
+++ ConnectedColors/ColorSwitchViewController.swift
@@ -4,16 +4,21 @@ class ColorSwitchViewController: UIViewController {
 
     @IBOutlet weak var connectionsLabel: UILabel!
 
+    let colorService = ColorService()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        colorService.delegate = self
     }
 
     @IBAction func redTapped() {
         self.change(color: .red)
+        colorService.send(colorName: "red")
     }
 
     @IBAction func yellowTapped() {
         self.change(color: .yellow)
+        colorService.send(colorName: "yellow")
     }
 
     func change(color : UIColor) {
@@ -22,4 +27,27 @@ class ColorSwitchViewController: UIViewController {
         }
     }
     
+}
+
+extension ColorSwitchViewController : ColorServiceDelegate {
+
+    func connectedDevicesChanged(manager: ColorService, connectedDevices: [String]) {
+        OperationQueue.main.addOperation {
+            self.connectionsLabel.text = "Connections: \(connectedDevices)"
+        }
+    }
+
+    func colorChanged(manager: ColorService, colorString: String) {
+        OperationQueue.main.addOperation {
+            switch colorString {
+            case "red":
+                self.change(color: .red)
+            case "yellow":
+                self.change(color: .yellow)
+            default:
+                NSLog("%@", "Unknown color value received: \(colorString)")
+            }
+        }
+    }
+
 }
